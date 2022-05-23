d088ecae5fb08a80384e4daa482d72e58673a0ae
alexcarantona <info@alejandrocarantonna.es>
2022-05-24 00:16:24 +0200

    added cypress test

--- cypress/integration/pokemon.js
+++ cypress/integration/pokemon.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
       cy.contains('ivysaur')
       cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
     })
+
+    it('user can navigate to one of the pages', function() {
+        cy.visit('http://localhost:5003')
+        cy.contains('ivysaur').click()
+        cy.url().should('match', /pokemon\/ivysaur$/)
+        cy.get(':nth-child(1) > .pokemon-ability-name').contains('overgrow')
+        cy.get(':nth-child(2) > .pokemon-ability-name').contains('chlorophyll')
+    })
   })
